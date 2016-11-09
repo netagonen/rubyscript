3d5405e7262b1df72b008f5cf0e66fc9a8d983ce
rails -v <netagonen@gmail.com>
2016-11-09 20:16:08 +0100

    exercise4

--- taskme.rb
+++ taskme.rb
@@ -1,16 +1,18 @@
-print "Give me a line please"
-input = get.chomp
+print "Give me a line or multiple lines seperated by a comma"
+input = gets.chomp.split(",")
 
 def cool_song (input)
-  puts input
   puts "\"I want a dyke for president"
+  input.each { |i| puts i }
   puts "..."
   puts "I want a Black woman for president"
   puts "I want someone with bad teeth..."
-  puts "Zoe Leonard"
+  print "What is your name?"
+  name = gets.chomp
+  puts "Zoe Leonard &#{name}"
   puts "1992\""
 end
 
-cool_song
+cool_song(input)
 
 #these programs print this cool song, one with quotes and one without
