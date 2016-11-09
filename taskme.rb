af67f8342b8cb3a2edefe566fb1fbd6afbfade02
rails -v <netagonen@gmail.com>
2016-11-09 19:36:44 +0100

    exercise2

--- taskme.rb
+++ taskme.rb
@@ -8,3 +8,14 @@ def cool_song
 end
 
 cool_song
+
+def cool_song1
+  puts "\"I want a dyke for president"
+  puts "..."
+  puts "I want a Black woman for president"
+  puts "I want someone with bad teeth..."
+  puts "Zoe Leonard"
+  puts "1992\""
+end
+
+cool_song1
