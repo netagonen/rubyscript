b2b080930c7cd0b54081e181000b3c898aa6523b
rails -v <netagonen@gmail.com>
2016-11-09 19:23:43 +0100

    i this is a ruby excercise

---
+++ taskme.rb
@@ -0,0 +1,10 @@
+def cool_song
+  puts "I want a dyke for president"
+  puts "..."
+  puts "I want a Black woman for president"
+  puts "I want someone with bad teeth..."
+  puts "Zoe Leonard"
+  puts "1992"
+end
+
+cool_song
